c44460170c01e64211b9669f8d26b319582b3b48
Yorai Levi <yorai.email@gmail.com>
2023-09-07 07:15:25 +0300

    Finished?

---
+++ src/training_detr.py
@@ -0,0 +1,78 @@
+training
+{'eval_loss': 8.161402702331543,
+ 'eval_map': 0.0021746535785496235,
+ 'eval_map_50': 0.0044554453343153,
+ 'eval_map_75': 0.00043611504952423275,
+ 'eval_map_small': 0.0021746535785496235,
+ 'eval_map_medium': -1.0,
+ 'eval_map_large': -1.0,
+ 'eval_mar_1': 0.009707602672278881,
+ 'eval_mar_10': 0.02766081877052784,
+ 'eval_mar_100': 0.02766081877052784,
+ 'eval_mar_small': 0.02766081877052784,
+ 'eval_mar_medium': -1.0,
+ 'eval_mar_large': -1.0,
+ 'eval_map_per_class': [0.006930693052709103,
+  -1.0,
+  0.0039425743743777275,
+  0.0,
+  0.0,
+  0.0],
+ 'eval_mar_100_per_class': [0.02777777798473835,
+  -1.0,
+  0.11052631586790085,
+  0.0,
+  0.0,
+  0.0],
+ 'eval_classes': [0, 1, 2, 3, 4, 6],
+ 'eval_runtime': 2.2189,
+ 'eval_samples_per_second': 7.661,
+ 'eval_steps_per_second': 2.253}
+
+evaluate 
+{'eval_loss': 3.276792287826538,
+ 'eval_map': 0.2896674573421478,
+ 'eval_map_50': 0.4256760776042938,
+ 'eval_map_75': 0.3128015697002411,
+ 'eval_map_small': 0.2896674573421478,
+ 'eval_map_medium': -1.0,
+ 'eval_map_large': -1.0,
+ 'eval_mar_1': 0.24191942811012268,
+ 'eval_mar_10': 0.36373621225357056,
+ 'eval_mar_100': 0.36373621225357056,
+ 'eval_mar_small': 0.36373621225357056,
+ 'eval_mar_medium': -1.0,
+ 'eval_mar_large': -1.0,
+ 'eval_map_per_class': [0.34106799960136414,
+  0.27839767932891846,
+  0.15955209732055664,
+  0.26130029559135437,
+  0.37770920991897583,
+  0.4389726519584656,
+  0.17067232728004456],
+ 'eval_mar_100_per_class': [0.41694074869155884,
+  0.34951767325401306,
+  0.2417665719985962,
+  0.33406493067741394,
+  0.4649662375450134,
+  0.4857916831970215,
+  0.25310570001602173],
+ 'eval_classes': [0, 1, 2, 3, 4, 5, 6],
+ 'eval_runtime': 235.759,
+ 'eval_samples_per_second': 3.881,
+ 'eval_steps_per_second': 0.488,
+ 'epoch': 100.0}
+
+IoU metric: bbox
+ Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.035
+ Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ] = 0.069
+ Average Precision  (AP) @[ IoU=0.75      | area=   all | maxDets=100 ] = 0.000
+ Average Precision  (AP) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = -1.000
+ Average Precision  (AP) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = -1.000
+ Average Precision  (AP) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.035
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=  1 ] = 0.083
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets= 10 ] = 0.083
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.083
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = -1.000
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = -1.000
+ Average Recall     (AR) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.083
